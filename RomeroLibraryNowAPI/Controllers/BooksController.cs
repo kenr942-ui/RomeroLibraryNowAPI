d6a0ab71f2d63c7a4b639f76b0e0d2fcc1329f22
STUDENT 17 <STUDENT 17@DESKTOP-R74NTU9>
2026-05-02 17:51:58 +0800

    Add project files.

---
+++ RomeroLibraryNowAPI/Controllers/BooksController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Mvc;
+using RomeroLibraryNowAPI.Models;
+using System.Reflection;
+
+namespace RomeroLibraryNowAPI.Controllers
+{
+    [Route("api/v1/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private static List<Book> books = new List<Book>
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Noli Me Tangere",
+                Author = "Dr. José Rizal",
+                Genre = "Novel",
+                Available = true,
+                PublishedYear = 1887
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "El Filibusterismo",
+                Author = "Dr. José Rizal",
+                Genre = "Novel",
+                Available = true,
+                PublishedYear = 1891
+            }
+
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(new
+            { status = "sucess",
+            data = books,
+            message ="books Retrieved."
+            });
+            
+        }
+        [HttpGet("{id}")]  
+        public IActionResult GetById(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound(new
+                {
+                    status = "error",
+                    data = (object?)null,
+                    message = "Book not found."
+                });
+            return Ok(new
+            {
+                status = "success",
+                data = book,
+                message = "Book retrieved."
+            });
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] Book newBook)
+        {
+            newBook.Id = books.Count + 1;
+            books.Add(newBook);
+            return CreatedAtAction(nameof(GetById),
+                new { id = newBook.Id },
+                new
+                {
+                    status = "success",
+                    data = newBook,
+                    message = "Book created."
+                });
+
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Book updateBook)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound(new
+                {
+                    status = "error",
+                    data = (object?)null,
+                    message = "Book not found."
+                });
+            book.Title = updateBook.Title;
+            book.Author = updateBook.Author;
+            book.Genre = updateBook.Genre;
+            book.Available = updateBook.Available;
+            book.PublishedYear = updateBook.PublishedYear;
+
+            return Ok(new
+            {
+                status = "success",
+                data = book,
+                message = "Book updated."
+            });
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+                if (book == null)
+                return NotFound(new
+                {
+                    status = "error",
+                    data = (object?)null,
+                    message = "Book not found."
+                });
+                books.Remove(book);
+            return Ok(new
+            {
+                status = "success",
+                data = (object?)null,
+                message = "Book deleted."
+            });
+        }
+    }
+}
+    
